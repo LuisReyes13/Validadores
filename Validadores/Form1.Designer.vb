c16ca31d8664e1a752538607c947e7aae1aea3bd
Luis Reyes <luis_a_reyes13@hotmail.com>
2024-05-01 18:47:48 -0600

    Agregar archivos de proyecto.

---
+++ Validadores/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtCP = New TextBox()
+        btnAceptar = New Button()
+        txtRFC = New TextBox()
+        Label2 = New Label()
+        cmbRegimenFiscal = New ComboBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        cmbCDFI = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(100, 49)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(45, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "C.P.:"
+        ' 
+        ' txtCP
+        ' 
+        txtCP.Location = New Point(151, 46)
+        txtCP.Name = "txtCP"
+        txtCP.Size = New Size(150, 31)
+        txtCP.TabIndex = 1
+        ' 
+        ' btnAceptar
+        ' 
+        btnAceptar.Location = New Point(413, 342)
+        btnAceptar.Name = "btnAceptar"
+        btnAceptar.Size = New Size(112, 34)
+        btnAceptar.TabIndex = 2
+        btnAceptar.Text = "Aceptar"
+        btnAceptar.UseVisualStyleBackColor = True
+        ' 
+        ' txtRFC
+        ' 
+        txtRFC.Location = New Point(151, 83)
+        txtRFC.Name = "txtRFC"
+        txtRFC.Size = New Size(363, 31)
+        txtRFC.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(98, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(47, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "RFC:"
+        ' 
+        ' cmbRegimenFiscal
+        ' 
+        cmbRegimenFiscal.FormattingEnabled = True
+        cmbRegimenFiscal.Location = New Point(151, 120)
+        cmbRegimenFiscal.Name = "cmbRegimenFiscal"
+        cmbRegimenFiscal.Size = New Size(363, 33)
+        cmbRegimenFiscal.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(13, 123)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(132, 25)
+        Label3.TabIndex = 6
+        Label3.Text = "Regimen Fiscal:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(30, 162)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(115, 25)
+        Label4.TabIndex = 8
+        Label4.Text = "Uso de CFDI:"
+        ' 
+        ' cmbCDFI
+        ' 
+        cmbCDFI.FormattingEnabled = True
+        cmbCDFI.Location = New Point(151, 159)
+        cmbCDFI.Name = "cmbCDFI"
+        cmbCDFI.Size = New Size(363, 33)
+        cmbCDFI.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(cmbCDFI)
+        Controls.Add(Label3)
+        Controls.Add(cmbRegimenFiscal)
+        Controls.Add(txtRFC)
+        Controls.Add(Label2)
+        Controls.Add(btnAceptar)
+        Controls.Add(txtCP)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCP As TextBox
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents txtRFC As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbRegimenFiscal As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents cmbCDFI As ComboBox
+
+End Class
---
+++ Validadores/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        If Not ValidarCodigoPostal(txtCP.Text) Then
+            MsgBox("El codigo postal debe contener 5 digitos numericos")
+            txtCP.Focus()
+        End If
+
+        If Not ValidarRFC(txtRFC.Text) Then
+            MsgBox("RFC no valido")
+            txtRFC.Focus()
+        End If
+
+    End Sub
+
+    Public Function ValidarCodigoPostal(numero As String) As Boolean
+        If Not System.Text.RegularExpressions.Regex.IsMatch(numero, "^[0-9]{5}$") Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Public Function ValidarRFC(rfc As String) As Boolean
+        If Not System.Text.RegularExpressions.Regex.IsMatch(rfc, "^([A-ZÑ&]{3,4}) ?(?:[A-Z]{1}) ?(?:[0-9]{2})(?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])(?:[A-Z0-9]{3})?$") Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub LlenarComboBoxRegimenFiscal()
+        ' Creamos una lista con los tipos de régimen fiscal.
+        Dim tiposRegimenFiscal As New List(Of String)
+        tiposRegimenFiscal.Add("Régimen General de Ley Personas Morales")
+        tiposRegimenFiscal.Add("Personas Morales con Fines no Lucrativos")
+        tiposRegimenFiscal.Add("Sueldos y Salarios e Ingresos Asimilados a Salarios")
+        tiposRegimenFiscal.Add("Régimen de Actividades Empresariales y Profesionales")
+        tiposRegimenFiscal.Add("Régimen de Incorporación Fiscal")
+        tiposRegimenFiscal.Add("Régimen Simplificado de Confianza")
+
+        ' Llenamos el ComboBox con los tipos de régimen fiscal.
+        cmbRegimenFiscal.DataSource = tiposRegimenFiscal
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LlenarComboBoxRegimenFiscal()
+    End Sub
+
+    Private Sub cmbRegimenFiscal_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbRegimenFiscal.SelectedIndexChanged
+        LlenarComboUsoCFDI(cmbRegimenFiscal.SelectedIndex)
+    End Sub
+
+    Private Sub LlenarComboUsoCFDI(Regimen As Integer)
+        Dim usoCFDI As New List(Of String)
+
+        Select Case Regimen
+            Case 1 ' Régimen General de Ley Personas Morales
+                usoCFDI.Add("Adquisición de mercancías")
+                usoCFDI.Add("Devoluciones, descuentos o bonificaciones")
+                usoCFDI.Add("Gastos en general")
+                usoCFDI.Add("Rentas")
+                usoCFDI.Add("Honorarios")
+                usoCFDI.Add("Retenciones")
+                usoCFDI.Add("Pagos")
+                usoCFDI.Add("Intermediación de pagos")
+
+            Case 2 ' Personas Morales con Fines no Lucrativos
+                usoCFDI.Add("Adquisición de mercancías")
+                usoCFDI.Add("Devoluciones, descuentos o bonificaciones")
+                usoCFDI.Add("Gastos en general")
+                usoCFDI.Add("Honorarios")
+                usoCFDI.Add("Rentas")
+                usoCFDI.Add("Retenciones")
+                usoCFDI.Add("Pagos")
+                usoCFDI.Add("Intermediación de pagos")
+
+            Case 3 ' Sueldos y Salarios e Ingresos Asimilados a Salarios
+                usoCFDI.Add("Honorarios médicos, dentales y gastos hospitalarios")
+                usoCFDI.Add("Gastos médicos por incapacidad o discapacidad")
+                usoCFDI.Add("Gastos funerales")
+                usoCFDI.Add("Donativos")
+                usoCFDI.Add("Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)")
+                usoCFDI.Add("Aportaciones voluntarias al SAR")
+                usoCFDI.Add("Primas por seguros de gastos médicos")
+                usoCFDI.Add("Gastos de transportación escolar obligatoria")
+                usoCFDI.Add("Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones")
+                usoCFDI.Add("Pagos por servicios educativos (colegiaturas)")
+                usoCFDI.Add("Sin efectos fiscales")
+                usoCFDI.Add("Pagos")
+                usoCFDI.Add("Nómina")
+
+            Case 4 ' Régimen de Actividades Empresariales y Profesionales
+                usoCFDI.Add("Adquisición de mercancías")
+                usoCFDI.Add("Devoluciones, descuentos o bonificaciones")
+                usoCFDI.Add("Gastos en general")
+                usoCFDI.Add("Construcciones")
+                usoCFDI.Add("Mobiliario y equipo de oficina por inversiones")
+                usoCFDI.Add("Equipo de transporte")
+                usoCFDI.Add("Equipo de computo y accesorios")
+                usoCFDI.Add("Dados, troqueles, moldes, matrices y herramental")
+                usoCFDI.Add("Comunicaciones telefónicas")
+                usoCFDI.Add("Comunicaciones satelitales")
+                usoCFDI.Add("Otra maquinaria y equipo")
+                usoCFDI.Add("Honorarios médicos, dentales y gastos hospitalarios")
+                usoCFDI.Add("Gastos médicos por incapacidad o discapacidad")
+                usoCFDI.Add("Gastos funerales")
+                usoCFDI.Add("Donativos")
+                usoCFDI.Add("Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)")
+                usoCFDI.Add("Aportaciones voluntarias al SAR")
+                usoCFDI.Add("Primas por seguros de gastos médicos")
+                usoCFDI.Add("Gastos de transportación escolar obligatoria")
+                usoCFDI.Add("Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones")
+                usoCFDI.Add("Pagos por servicios educativos (colegiaturas)")
+                usoCFDI.Add("Sin efectos fiscales")
+                usoCFDI.Add("Pagos")
+
+            Case 5 ' Régimen de Incorporación Fiscal
+                usoCFDI.Add("Adquisición de mercancías")
+                usoCFDI.Add("Devoluciones, descuentos o bonificaciones")
+                usoCFDI.Add("Gastos en general")
+                usoCFDI.Add("Construcciones")
+                usoCFDI.Add("Mobiliario y equipo de oficina por inversiones")
+                usoCFDI.Add("Equipo de transporte")
+                usoCFDI.Add("Equipo de computo y accesorios")
+                usoCFDI.Add("Dados, troqueles, moldes, matrices y herramental")
+                usoCFDI.Add("Comunicaciones telefónicas")
+                usoCFDI.Add("Comunicaciones satelitales")
+                usoCFDI.Add("Otra maquinaria y equipo")
+                usoCFDI.Add("Sin efectos fiscales")
+                usoCFDI.Add("Pagos")
+
+            Case 6 ' Régimen Simplificado de Confianza
+                usoCFDI.Add("Adquisición de mercancías")
+                usoCFDI.Add("Devoluciones, descuentos o bonificaciones")
+                usoCFDI.Add("Gastos en general")
+                usoCFDI.Add("Construcciones")
+                usoCFDI.Add("Mobiliario y equipo de oficina por inversiones")
+                usoCFDI.Add("Equipo de transporte")
+                usoCFDI.Add("Equipo de computo y accesorios")
+                usoCFDI.Add("Dados, troqueles, moldes, matrices y herramental")
+                usoCFDI.Add("Comunicaciones telefónicas")
+                usoCFDI.Add("Comunicaciones satelitales")
+                usoCFDI.Add("Otra maquinaria y equipo")
+                usoCFDI.Add("Sin efectos fiscales")
+                usoCFDI.Add("Pagos")
+
+        End Select
+
+        cmbCDFI.DataSource = usoCFDI
+
+    End Sub
+End Class
